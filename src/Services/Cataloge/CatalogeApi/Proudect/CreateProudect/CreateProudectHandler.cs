749f53126b6582bbb6642311cd622bbba97ecfee
MICHAEL-MAURICE <Michaelmauricengn@gmail.com>
2025-02-11 00:03:44 +0200

    Update project references and refactor command handler
    Updated BuildingBlocks.csproj to include Carter 8.0.0 and Mapster 7.4.0. Modified CatalogeApi.csproj to remove MediatR, add Carter 8.0.0, Microsoft.VisualStudio.Azure.Containers.Tools.Targets 1.21.0, and reference BuildingBlocks.csproj. Refactored CreateProudectHandler.cs to use ICommand and ICommandHandler from BuildingBlocks.CQRS.

--- src/Services/Cataloge/CatalogeApi/Proudect/CreateProudect/CreateProudectHandler.cs
+++ src/Services/Cataloge/CatalogeApi/Proudect/CreateProudect/CreateProudectHandler.cs
@@ -1,9 +1,11 @@
-﻿using MediatR;
+﻿using System.Windows.Input;
+using BuildingBlocks.CQRS;
+using MediatR;
 namespace CatalogeApi.Proudect.CreateProudect;
 public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
-    : IRequest<CreateProductResult>;
+    : ICommand<CreateProductResult>;
 public record CreateProductResult(Guid Id);
-public class CreateProudectHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
+public class CreateProudectHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
 {
     public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
     {
